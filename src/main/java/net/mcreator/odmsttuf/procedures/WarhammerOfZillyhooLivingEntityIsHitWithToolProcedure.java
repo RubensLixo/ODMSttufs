d0b4ab1f2ba3a2868cf5577851d6ff8cdb293757
olaska-max <Usuario@DESKTOP-IP2CC5U>
2025-02-15 16:08:29 -0300

    WHOZ melhorado

--- src/main/java/net/mcreator/odmsttuf/procedures/WarhammerOfZillyhooLivingEntityIsHitWithToolProcedure.java
+++ src/main/java/net/mcreator/odmsttuf/procedures/WarhammerOfZillyhooLivingEntityIsHitWithToolProcedure.java
@@ -1,12 +1,14 @@
 package net.mcreator.odmsttuf.procedures;
 
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.core.registries.Registries;
 
 public class WarhammerOfZillyhooLivingEntityIsHitWithToolProcedure {
@@ -18,7 +20,14 @@ public class WarhammerOfZillyhooLivingEntityIsHitWithToolProcedure {
 				_entity.forceAddEffect(new MobEffectInstance(MobEffects.LEVITATION, 1200, 2, false, false), entity);
 		}
 		if (Math.random() < 0.005) {
-			entity.hurt(new DamageSource(world.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.INDIRECT_MAGIC)), (float) Double.POSITIVE_INFINITY);
+			for (int index0 = 0; index0 < 20; index0++) {
+				entity.hurt(new DamageSource(world.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(ResourceKey.create(Registries.DAMAGE_TYPE, new ResourceLocation("odmsttuf:godtierdamage")))),
+						(float) Double.POSITIVE_INFINITY);
+			}
+		}
+		if (Math.random() < 0.0001) {
+			if (entity instanceof Player _player)
+				_player.getInventory().clearContent();
 		}
 	}
 }
