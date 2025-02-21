5b35464d0041bf6f69450a81e73848e279c82aad
Builder's Deams <Dartanham@servidor>
2025-02-21 11:50:12 -0300

    removido item test

--- src/main/java/net/mcreator/odmsttuf/init/OdmsttufModItems.java
+++ src/main/java/net/mcreator/odmsttuf/init/OdmsttufModItems.java
@@ -13,7 +13,6 @@ import net.minecraft.world.item.Item;
 import net.mcreator.odmsttuf.item.WaterPickaxeItem;
 import net.mcreator.odmsttuf.item.WarhammerOfZillyhooItem;
 import net.mcreator.odmsttuf.item.SteampunkGlassesItem;
-import net.mcreator.odmsttuf.item.IItem;
 import net.mcreator.odmsttuf.item.CursedArtifactItem;
 import net.mcreator.odmsttuf.item.AItem;
 import net.mcreator.odmsttuf.OdmsttufMod;
@@ -25,7 +24,6 @@ public class OdmsttufModItems {
 	public static final RegistryObject<Item> STEAMPUNK_GLASSES = REGISTRY.register("steampunk_glasses", () -> new SteampunkGlassesItem());
 	public static final RegistryObject<Item> A_HELMET = REGISTRY.register("a_helmet", () -> new AItem.Helmet());
 	public static final RegistryObject<Item> WATER_PICKAXE = REGISTRY.register("water_pickaxe", () -> new WaterPickaxeItem());
-	public static final RegistryObject<Item> I = REGISTRY.register("i", () -> new IItem());
 	// Start of user code block custom items
 	// End of user code block custom items
 }
