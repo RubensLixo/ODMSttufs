236fdc1190ffaadfbbb2587a00ec473d01948872
Builder's Deams <Dartanham@servidor>
2025-02-28 20:56:56 -0300

    resync

--- src/main/java/net/mcreator/odmsttuf/procedures/AHelmetTickEventProcedure.java
+++ src/main/java/net/mcreator/odmsttuf/procedures/AHelmetTickEventProcedure.java
@@ -1,10 +1,19 @@
 package net.mcreator.odmsttuf.procedures;
 
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.damagesource.DamageTypes;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.core.particles.ParticleTypes;
+
+import net.mcreator.odmsttuf.network.OdmsttufModVariables;
+import net.mcreator.odmsttuf.OdmsttufMod;
 
 public class AHelmetTickEventProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
@@ -17,6 +26,13 @@ public class AHelmetTickEventProcedure {
 		if (entity instanceof Player player && player.getAbilities().flying) {
 			if (world instanceof ServerLevel _level)
 				_level.sendParticles(ParticleTypes.SPLASH, x, (y - 1), z, 25, 0, 0, 0, 1);
+		}
+		if (!(OdmsttufModVariables.MapVariables.get(world).WaterReviveCount == 0)) {
+			if (!((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == Items.WATER_BUCKET)) {
+				OdmsttufMod.queueServerWork(40, () -> {
+					entity.hurt(new DamageSource(world.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.DROWN)), 2);
+				});
+			}
 		}
 	}
 }
--- src/main/java/net/mcreator/odmsttuf/procedures/ReviveProcedure.java
+++ src/main/java/net/mcreator/odmsttuf/procedures/ReviveProcedure.java
@@ -41,34 +41,7 @@ public class ReviveProcedure {
 				if (entity instanceof LivingEntity _entity)
 					_entity.setHealth((float) ((entity instanceof LivingEntity _livEnt ? _livEnt.getMaxHealth() : -1) / 2));
 			}
-			if (!damagesource.is(DamageTypes.MOB_ATTACK)) {
-				if (event != null && event.isCancelable()) {
-					event.setCanceled(true);
-				} else if (event != null && event.hasResult()) {
-					event.setResult(Event.Result.DENY);
-				}
-				if (entity instanceof LivingEntity _entity)
-					_entity.setHealth((float) ((entity instanceof LivingEntity _livEnt ? _livEnt.getMaxHealth() : -1) / 2));
-			}
 			if (!damagesource.is(DamageTypes.PLAYER_EXPLOSION)) {
-				if (event != null && event.isCancelable()) {
-					event.setCanceled(true);
-				} else if (event != null && event.hasResult()) {
-					event.setResult(Event.Result.DENY);
-				}
-				if (entity instanceof LivingEntity _entity)
-					_entity.setHealth((float) ((entity instanceof LivingEntity _livEnt ? _livEnt.getMaxHealth() : -1) / 2));
-			}
-			if (!damagesource.is(DamageTypes.MOB_PROJECTILE)) {
-				if (event != null && event.isCancelable()) {
-					event.setCanceled(true);
-				} else if (event != null && event.hasResult()) {
-					event.setResult(Event.Result.DENY);
-				}
-				if (entity instanceof LivingEntity _entity)
-					_entity.setHealth((float) ((entity instanceof LivingEntity _livEnt ? _livEnt.getMaxHealth() : -1) / 2));
-			}
-			if (!damagesource.is(DamageTypes.MAGIC)) {
 				if (event != null && event.isCancelable()) {
 					event.setCanceled(true);
 				} else if (event != null && event.hasResult()) {
@@ -88,34 +61,7 @@ public class ReviveProcedure {
 				if (entity instanceof LivingEntity _entity)
 					_entity.setHealth((float) ((entity instanceof LivingEntity _livEnt ? _livEnt.getMaxHealth() : -1) / 2));
 			}
-			if (!damagesource.is(DamageTypes.MOB_ATTACK)) {
-				if (event != null && event.isCancelable()) {
-					event.setCanceled(true);
-				} else if (event != null && event.hasResult()) {
-					event.setResult(Event.Result.DENY);
-				}
-				if (entity instanceof LivingEntity _entity)
-					_entity.setHealth((float) ((entity instanceof LivingEntity _livEnt ? _livEnt.getMaxHealth() : -1) / 2));
-			}
 			if (!damagesource.is(DamageTypes.PLAYER_EXPLOSION)) {
-				if (event != null && event.isCancelable()) {
-					event.setCanceled(true);
-				} else if (event != null && event.hasResult()) {
-					event.setResult(Event.Result.DENY);
-				}
-				if (entity instanceof LivingEntity _entity)
-					_entity.setHealth((float) ((entity instanceof LivingEntity _livEnt ? _livEnt.getMaxHealth() : -1) / 2));
-			}
-			if (!damagesource.is(DamageTypes.MOB_PROJECTILE)) {
-				if (event != null && event.isCancelable()) {
-					event.setCanceled(true);
-				} else if (event != null && event.hasResult()) {
-					event.setResult(Event.Result.DENY);
-				}
-				if (entity instanceof LivingEntity _entity)
-					_entity.setHealth((float) ((entity instanceof LivingEntity _livEnt ? _livEnt.getMaxHealth() : -1) / 2));
-			}
-			if (!damagesource.is(DamageTypes.MAGIC)) {
 				if (event != null && event.isCancelable()) {
 					event.setCanceled(true);
 				} else if (event != null && event.hasResult()) {
